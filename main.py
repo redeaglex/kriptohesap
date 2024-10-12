6c360d119ab77cdf69da7ffa33dcc91144194c6d
redeaglex <zeynepbulut1233@gmail.com>
2024-10-12 11:50:49 +0300

    kripto hesaplama
    gerçekten çeviriyor

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import requests
+
+r = requests.get('https://api.coinlore.net/api/tickers/')
+veri = r.json()
+
+type(veri)
+
+coinler = veri['data']
+
+coinfiyat = {}
+
+for coin in coinler:
+    sembol = coin['symbol']
+    fiyat = coin['price_usd']
+
+    coinfiyat.update({sembol: float(fiyat)})
+
+coinisimler = coinfiyat.keys()
+
+coin1=st.sidebar.selectbox("Eldeki Coin",coinisimler)
+miktar=st.sidebar.number_input("Miktar")
+coin2=st.sidebar.selectbox("Hedef coin",coinisimler)
+
+
+
+c1 = coinfiyat.get(coin1)
+miktar = 20
+c2 = coinfiyat.get(coin2)
+sonuc=miktar * c1 / c2
+
+st.write(miktar,"adet",coin1,sonuc,"adet",coin2)
+st.link_button("Satın Al",f"https://www.coinbase.com/tr/converter/{coin1}/{coin2}")
